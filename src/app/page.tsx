30f601921fbe5347770510c3090bce8b53fcd375
devmorwal03 <20ee02011@iitbbs.ac.in>
2026-02-12 13:11:11 +0530

    code and database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@base-ui/react";
 
 export default function Home() {
   return (
-    <div className="text-4xl font-bold text-green-500">
-      Hello World!
-    </div>
+    <Button>
+      click name
+    </Button>
   )
 }
