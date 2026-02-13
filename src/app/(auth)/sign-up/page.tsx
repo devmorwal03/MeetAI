731976b2d59e6682ebde38df1688335b64a90ae5
devmorwal03 <20ee02011@iitbbs.ac.in>
2026-02-13 15:38:53 +0530

    authentiationui

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@/components/ui/card"
+import { SignUpView } from "@/modules/auth/ui/view/sign-up-view"
+
+const Page = () => {
+    console.log("Sign Up page")
+    return (
+        <SignUpView/>
+    )
+
+}
+
+export default Page
