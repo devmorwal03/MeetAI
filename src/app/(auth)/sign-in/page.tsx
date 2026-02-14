1c665ea53bf456b6b857924f0c34f44b25b26a84
devmorwal03 <20ee02011@iitbbs.ac.in>
2026-02-14 15:13:09 +0530

    authencication socials

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,20 @@
 import { SignInView } from "@/modules/auth/ui/view/sign-in-view"
+import { auth } from "@/lib/auth"
+import { redirect } from "next/navigation"
+import { headers } from "next/headers"
 
-const Page = () => {
-    console.log("Sign in page")
+const Page = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    })
+
+    if (session) {
+        redirect("/")
+    }
+
+    
     return (
-        <SignInView/>
+        <SignInView />
     )
 }
 
